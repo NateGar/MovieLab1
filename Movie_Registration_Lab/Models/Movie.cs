31b858051a6a79842bef439c701aa800fe985cb4
[Nate] <[ncggardner@gmail.com]>
2020-05-21 16:48:41 -0400

    Movie Lab first commit

---
+++ Movie_Registration_Lab/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Registration_Lab.Models
+{
+    public class Movie
+    {
+        
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Year { get; set; }
+
+        public string Actors { get; set; }
+
+        public string Directors { get; set; }
+    }
+}
